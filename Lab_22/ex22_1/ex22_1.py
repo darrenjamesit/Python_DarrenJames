f516d6260a770275c0d64c41646acb87c5adcddf
Darren James <darrenjames.it@gmail.com>
2023-03-03 12:52:59 +0200

    added main page

--- Lab_22/ex22_1/ex22_1.py
+++ Lab_22/ex22_1/ex22_1.py
@@ -9,9 +9,21 @@ st = {
 }
 
 
+@app.route('/')
+@app.route('/home/')
+def home():
+    return "This is the main page."
+
+
 @app.route('/students/')
-def show_students():
-    return render_template('students.html', students=st)
+def all_students():
+    return render_template('students.html', title='Students', students=st)
+
+
+@app.route('/student/<int:stud_id>')
+def student(stud_id):
+    return render_template('stud_id.html', students=st, stud_id=stud_id)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
