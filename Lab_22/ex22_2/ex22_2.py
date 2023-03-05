d4b2dbfff0b39464273bfa24ff0825a45d5edeec
Darren James <darrenjames.it@gmail.com>
2023-03-05 16:16:38 +0200

    copy of all ex22_1 files

---
+++ Lab_22/ex22_2/ex22_2.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+app = Flask('Students')
+
+st = {
+    23: {'fname': 'Dana', 'lname': 'Popescu', 'class': '9b'},
+    2: {'fname': 'Ion', 'lname': 'Pop', 'class': '10c'},
+    31: {'fname': 'Gelu', 'lname': 'Ionescu', 'class': '10c'},
+    15: {'fname': 'Geta', 'lname': 'Ionescu', 'class': '9b'},
+}
+
+
+@app.route('/')
+@app.route('/home/')
+def home():
+    return "This is the main page."
+
+
+@app.route('/students/')
+def all_students():
+    return render_template('students.html', title='Students', students=st)
+
+
+@app.route('/student/<int:stud_id>')
+def student(stud_id):
+    return render_template('stud_id.html', students=st, stud_id=stud_id)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
