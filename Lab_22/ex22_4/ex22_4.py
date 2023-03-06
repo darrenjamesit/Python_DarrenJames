11d2b82cbbb69454b4e2ce28363bdedf267252b5
Darren James <darrenjames.it@gmail.com>
2023-03-06 18:46:57 +0200

    created ex22_4 and added grades to dictionary

---
+++ Lab_22/ex22_4/ex22_4.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+app = Flask('Students')
+
+st = {
+    23: {'fname': 'Dana', 'lname': 'Popescu', 'class': '9b', 'grades': [4, 6, 7]},
+    2: {'fname': 'Ion', 'lname': 'Pop', 'class': '10c', 'grades': [8, 5, 9]},
+    31: {'fname': 'Gelu', 'lname': 'Ionescu', 'class': '10c', 'grades': [6, 9, 8]},
+    15: {'fname': 'Geta', 'lname': 'Ionescu', 'class': '9b', 'grades': [10, 9, 10]},
+}
+x = list(st.values())
+y = [d['class'] for d in x]
+fnam = [e['fname'] for e in x]
+lnam = [f['lname'] for f in x]
+
+
+@app.route('/')
+@app.route('/home/')
+def home():
+    return "This is the main page."
+
+
+@app.route('/students/')
+def all_students():
+    return render_template('students.html', title='Students', students=st)
+
+
+@app.route('/student/<int:stud_id>')
+def student(stud_id):
+    return render_template('stud_id.html', students=st, stud_id=stud_id)
+
+
+@app.route('/class/<class_name>')
+def clas(class_name):
+    return render_template('class.html', students=st, class_name=class_name, x=x, y=y)
+
+
+@app.route('/search/', methods=['GET', 'POST'])
+def search():
+    print(request.form)
+    print(dict(request.form))
+    print(request.form.get('fname'))
+    print(request.form.get('lname'))
+    return render_template('search.html', search=request.form, students=st, fnam=fnam, lnam=lnam)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
