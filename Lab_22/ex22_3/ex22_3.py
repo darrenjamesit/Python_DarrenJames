d9722cbac74a8d114ae693059cec73b5c5908580
Darren James <darrenjames.it@gmail.com>
2023-03-06 15:24:51 +0200

    copy of ex22_2 as basis for ex22_3

---
+++ Lab_22/ex22_3/ex22_3.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+app = Flask('Students')
+
+st = {
+    23: {'fname': 'Dana', 'lname': 'Popescu', 'class': '9b'},
+    2: {'fname': 'Ion', 'lname': 'Pop', 'class': '10c'},
+    31: {'fname': 'Gelu', 'lname': 'Ionescu', 'class': '10c'},
+    15: {'fname': 'Geta', 'lname': 'Ionescu', 'class': '9b'},
+}
+x = list(st.values())
+y = [d['class'] for d in x]
+
+
+@app.route('/')
+@app.route('/home/')
+def home():
+    return "This is the main page."
+
+
+@app.route('/students/')
+def all_students():
+    return render_template('students.html', title='Students', students=st)
+
+
+@app.route('/student/<int:stud_id>')
+def student(stud_id):
+    return render_template('stud_id.html', students=st, stud_id=stud_id)
+
+
+@app.route('/class/<class_name>')
+def clas(class_name):
+    return render_template('class.html', students=st, class_name=class_name, x=x, y=y)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
